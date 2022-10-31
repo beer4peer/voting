0f9dc148abe785e4237973e575fd14b120e90b39
Nick Pratley <nick@npratley.net>
2022-11-01 00:53:37 +1100

    slack notifications on poll create

--- app/Notifications/PollCreated.php
+++ app/Notifications/PollCreated.php
@@ -33,7 +33,7 @@ class PollCreated extends Notification
                 "A new poll has been created " . "\r\n\r\n" .
                 "*Title:* {$this->poll->title}" . "\r\n" .
                 "*Category:* {$this->poll->category->name} " . "\r\n" .
-                "*Ends At: *{$ends_at}" . "\r\n" .
+                "*Ends At:* {$ends_at}" . "\r\n" .
                 "{$this->poll->description}". "\r\n\r\n" .
                 "Vote Here: {$url}" ."\r\n\r\n" .
                 "_Reminder: You are required to login via Slack to vote. This is required to stop abuse and duplicate votes. Voting is anonymous and we only store your name and Slack ID which is unique to beer4peer. Therefore once your vote has been registered you cannot change it. The source code is hiding on GitHub (https://github.com/beer4peer/voting) if you don't believe me._"
