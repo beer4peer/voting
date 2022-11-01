bf7423a37cdfad5ad2d2f0cc1913cec37cc9c936
Nick Pratley <nick@npratley.net>
2022-11-01 11:40:50 +1100

    spelling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -76,7 +76,7 @@
 
         <livewire:scripts />
         <footer class="fixed bottom-0 left-0 z-20 p-4 w-full bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
-        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">Built by <a href="https://theitdept.au/" class="hover:underline">The It Dept</a>.
+        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">Built by <a href="https://theitdept.au/" class="hover:underline">The IT Dept</a>.
         </span>
             <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
                 <li>
